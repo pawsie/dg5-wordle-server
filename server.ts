ba3e7917416bdae20848e2620fae5a5c522cbf85
pawsie <pawsielee@gmail.com>
2022-08-12 14:15:44 +0800

    add cors to allow graphql request from angular app

--- server.ts
+++ server.ts
@@ -4,9 +4,12 @@ import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import { schema, resolver } from './schema'
 import envs from './envs'
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use(
     envs.graphqlPath,
